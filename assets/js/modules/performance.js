14f68f27ef6ba85238ba2c39ba79cc999b7a5508
script26 <97182340+script26@users.noreply.github.com>
2026-01-04 23:11:48 -0700

    Create performance.js

---
+++ assets/js/modules/performance.js
@@ -0,0 +1,25 @@
+    // Good UI or faster version of site
+    if (navigator.deviceMemory < 2 || performance.now() > 3000) {
+      console.log("low memory: " + navigator.deviceMemory);
+      console.log("slow performance: " + performance.now());
+      removeVideoBackground();
+      
+      const elements = document.querySelectorAll('.blur');
+      elements.forEach((element, i) => {
+        element.classList.remove("blur");
+        element.classList.add("plain-color");
+      });
+      const performanceMode = document.getElementById('performanceMode');
+      performanceMode.classList.toggle('active');
+      setTimeout(() => performanceMode.classList.toggle('active'), 2000);     
+    } else {
+      console.log("high memory: " + navigator.deviceMemory);
+      console.log("fast performance: " + performance.now());
+      console.log("fast enough cpu");
+
+      const elements = document.querySelectorAll('.plain-color');
+      elements.forEach((element, i) => {
+        element.classList.remove("plain-color");
+        element.classList.add("blur");
+      });
+    }
