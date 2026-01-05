d3baade2983c82bc86f2bc7136eeda9d80ef5443
script26 <97182340+script26@users.noreply.github.com>
2026-01-04 23:16:52 -0700

    Update performance.js

--- assets/js/modules/performance.js
+++ assets/js/modules/performance.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function backgrounds() {
+function performance() {
     // Good UI or faster version of site
     if (navigator.deviceMemory < 2 || performance.now() > 3000) {
       console.log("low memory: " + navigator.deviceMemory);
